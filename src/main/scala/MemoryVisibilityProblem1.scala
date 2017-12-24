e528414a6d7af3510a2cc6ba06097debf58b13b4
Soichiro Yoshimura <yoshimura@soichiro.org>
2017-12-24 17:23:41 +0900

    Typo fix AsyncRunner2 => AsyncRunner1

--- src/main/scala/MemoryVisibilityProblem1.scala
+++ src/main/scala/MemoryVisibilityProblem1.scala
@@ -1,9 +1,9 @@
 object MemoryVisibilityProblem1 extends App {
-  var runner = new AsyncRunner2("Runner 0", () => true)
+  var runner = new AsyncRunner1("Runner 0", () => true)
 
   for (i <- 1 to 10) {
     runner.asyncRun((name) => println(s"${name} is finished."))
-    runner = new AsyncRunner2(s"Runner ${i}", runner.canNextStart)
+    runner = new AsyncRunner1(s"Runner ${i}", runner.canNextStart)
   }
 
   runner.asyncRun((name) => println(s"${name} is finished. Totally finished."))
