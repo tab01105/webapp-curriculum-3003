8a0c6b2137ed7f57b9aabf3d7105289b8a60e27f
tab01105 <tab01105@gmail.com>
2018-01-24 14:57:22 +0900

    ThreadSafeFormatterのスレッドセーフ化

--- src/main/scala/ThreadSafeFormatter.scala
+++ src/main/scala/ThreadSafeFormatter.scala
@@ -1,7 +1,11 @@
 import java.util.Date
+import java.text.SimpleDateFormat
 
 object ThreadSafeFormatter {
 
-  def format(date: Date) = ???
+  def format(date: Date): String = {
+    val formatter = new SimpleDateFormat("yyyy年MM月dd日E曜日HH時mm分")
+    formatter.format(date)
+  }
 
 }
